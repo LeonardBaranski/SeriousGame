be4e95001bff2811ade887c94d23ec1fdcbd96a4
LeonardBaranski <80849791+LeonardBaranski@users.noreply.github.com>
2022-05-24 12:26:44 +0200

    Aufgeräut und MovementSkript erzeugt

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey ("d")) {
+            pos.x += moveSpeed * Time.deltaTime;
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Input.GetKey ("a")) {
+            pos.x -= moveSpeed * Time.deltaTime;   
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        transform.position = pos;
+    }
+}
